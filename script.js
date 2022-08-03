38089ff31606ecb531b45fb2681225feebaf0cae
Dimitrije1986 <dimitrijekrnic@gmail.com>
2022-08-03 17:28:07 +0200

    added changes

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 //navbar//
+let btn = document.querySelector('.header button');
+let meni = document.querySelector('.header ul');
 
-const mobileMenu = () => {
-	let btn = document.querySelector('.header button');
-	let meni = document.querySelector('.header ul');
+btn.addEventListener('click', e => {
 
-	if(btn.innerText === 'MENU') {
+ if (btn.innerText === 'MENU') {
 		meni.style.display = 'block';
 		btn.innerText = 'CLOSE';
    }else{
         meni.style.display = 'none';
         btn.innerText = 'MENU';   
 		}
-}
+ 
+});
+
+window.addEventListener('resize', e => {
+	if (window.innerWidth > 1000) {
+	   meni.style.display = 'flex';
+     }else{
+	   meni.style.display = 'none';
+    }
+});
+
 
 //Galerija pocetna//
 
@@ -54,3 +64,4 @@ const displayNone = () => {
 		img.style.display = 'none';
 	})
 }
+
