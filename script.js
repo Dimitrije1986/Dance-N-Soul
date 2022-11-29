4caa0acd4cc253018dedc72ea5fa71ae39114a48
Dimitrije1986 <dimitrijekrnic@gmail.com>
2022-11-29 15:54:25 +0100

    adding popup-modal

--- script.js
+++ script.js
@@ -1,15 +1,51 @@
-//navbar//
-let link = document.querySelector(".menu");
-let meni = document.querySelector(".header ul");
+'use strict';
 
-link.addEventListener("click", (e) => {
-  meni.classList.toggle("open");
+//navbar//
+let link = document.querySelector('.menu');
+let meni = document.querySelector('.header ul');
+
+link.addEventListener('click', e => {
+  meni.classList.toggle('open');
 });
 
 //************ form ********** //
 
 window.onbeforeunload = () => {
-  for (const form of document.getElementsByTagName("form")) {
+  for (const form of document.getElementsByTagName('form')) {
     form.reset();
   }
 };
+
+//**********  popup-modal *********//
+
+const btnMirna = document.querySelector('.button-mirna');
+const btnSanja = document.querySelector('.button-sanja');
+const popupMirna = document.querySelector('.mirna');
+const popupSanja = document.querySelector('.sanja');
+
+const overlay = document.querySelector('.add');
+const closeBtns = document.querySelectorAll('.close-modal');
+
+const openModal = () => {
+  if (event.target.id === 'mirna') {
+    popupMirna.classList.remove('hidden');
+  }
+  if (event.target.id === 'sanja') {
+    popupSanja.classList.remove('hidden');
+  }
+  overlay.classList.add('overlay');
+};
+
+const closeModal = () => {
+  popupMirna.classList.add('hidden');
+  popupSanja.classList.add('hidden');
+  overlay.classList.remove('overlay');
+};
+
+btnMirna.addEventListener('click', openModal);
+btnSanja.addEventListener('click', openModal);
+
+for (let i = 0; i < closeBtns.length; i++)
+  closeBtns[i].addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
